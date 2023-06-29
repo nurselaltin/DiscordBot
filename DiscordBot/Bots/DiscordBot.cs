e4757c2cef12df8cf02c56d3287b7571afe7a77d
nurselaltin <nurselaltin.na@gmail.com>
2023-06-29 20:52:04 +0300

    Edited medium api and the project structure.

---
+++ DiscordBot/Bots/DiscordBot.cs
@@ -0,0 +1,64 @@
+﻿using DiscordBot.Utilities;
+using DSharpPlus;
+using DSharpPlus.Commands;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Extensions;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace DiscordBot.Bots
+{
+    public class DiscordBot
+    {
+        public DiscordClient Client { get; set; }
+        public InteractivityExtension Interactivity { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
+
+        public async Task RunAsyc()
+        {
+            //deseriliaze json
+            var json = string.Empty;
+            using (var fs = File.OpenRead("C:\\config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync();
+
+            var configJson = JsonConvert.DeserializeObject<ConfigJSON>(json);
+
+            var config = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = configJson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+            };
+
+            //Connect settings
+            Client = new DiscordClient(config);
+            Client.UseInteractivity(new InteractivityConfiguration()
+            {
+                Timeout = TimeSpan.FromMinutes(2)
+            });
+
+            //Bot Commands
+            var commandsConfig = new CommandsNextConfiguration()
+            {
+                StringPrefixes = new string[] { configJson.Prefix },
+                EnableMentionPrefix = true,
+                EnableDms = true,
+                EnableDefaultHelp = false,
+            };
+            Commands = Client.UseCommandsNext(commandsConfig);
+            Commands.RegisterCommands<FunCommands>();
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private Task OnClientReady(ReadyEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
--- DiscordBot/Bots/MediumBot.cs
+++ DiscordBot/Bots/MediumBot.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace DiscordBot.Bots
 {
   public class MediumBot
   {
+    public MediumBot()
+    {
+        //settings
+        //close console
+        ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+        service.HideCommandPromptWindow = true;
+
+        //close browser
+        ChromeOptions arg = new ChromeOptions();
+        arg.AddArguments("--headless");
+       
+        IWebDriver driver = new ChromeDriver(service, arg);
+        driver.Navigate().GoToUrl("https://medium.com/trendyol-tech");
+        var links = new List<string>();
+
+        var el = driver.FindElements(By.ClassName("row")).ToList();
+        foreach (var div in el)
+        {
+           var divs = div.FindElements(By.ClassName("u-xs-size12of12"));
+           foreach (var item in divs)
+           {
+              string link = item.FindElement(By.ClassName("u-xs-marginBottom10")).FindElement(By.TagName("a")).GetAttribute("href");
+              links.Add(link);
+           }
+        }
+     
+        if(links.Count() > 0)
+        {
+           File.WriteAllLines(@"D:\medium_links.txt", links);
+        }
+    }
   }
 }
